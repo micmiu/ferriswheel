1e811491efcb61b158e2ac8a11a7c1a2e296e9bc
micmiu <sjsky007@gmail.com>
2015-12-01 23:16:34 +0800

    update:support jqgrid nav edit operation

--- ferriswheel-support/ferriswheel-support-jqgrid/src/main/java/com/micmiu/mvc/ferriswheel/support/jqgrid/controller/JqgridManageController.java
+++ ferriswheel-support/ferriswheel-support-jqgrid/src/main/java/com/micmiu/mvc/ferriswheel/support/jqgrid/controller/JqgridManageController.java
@@ -1,11 +1,18 @@
 package com.micmiu.mvc.ferriswheel.support.jqgrid.controller;
 
+import com.micmiu.mvc.ferriswheel.core.FerriswheelConstant;
 import com.micmiu.mvc.ferriswheel.core.entity.FerriswheelID;
+import com.micmiu.mvc.ferriswheel.core.model.OperationType;
 import com.micmiu.mvc.ferriswheel.core.model.Page;
 import com.micmiu.mvc.ferriswheel.support.jqgrid.model.JqgridPage;
 import com.micmiu.mvc.ferriswheel.support.jqgrid.model.JqgridQuery;
 import com.micmiu.mvc.ferriswheel.support.spring.mvc.SpringBaseManageController;
 import org.apache.commons.beanutils.BeanUtils;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.Serializable;
@@ -28,6 +35,48 @@ public abstract class JqgridManageController<E extends FerriswheelID, V, ID exte
 	@Override
 	protected void parseQuery(HttpServletRequest request, Q query) {
 		//
+	}
+
+	@RequestMapping(params = {"oper=add"})
+	@ResponseBody
+	public String create(E entity, HttpServletRequest request) {
+		checkAuth(OperationType.CREATE.getValue());
+		if (this.handler4Create(request, entity)) {
+			getBaseService().create(entity);
+		}
+		String message = messageSource.getMessage(FerriswheelConstant.FW_MSG_SUCC,
+				null, RequestContextUtils.getLocale(request));
+		return message;
+	}
+
+	@RequestMapping(params = {"oper=edit"})
+	@ResponseBody
+	public String update(E entity, HttpServletRequest request) {
+		checkAuth(OperationType.UPDATE.getValue());
+		if (this.handler4Update(request, entity)) {
+			getBaseService().update(entity);
+		}
+		String message = messageSource.getMessage(FerriswheelConstant.FW_MSG_SUCC,
+				null, RequestContextUtils.getLocale(request));
+		return message;
+	}
+
+	@RequestMapping(params = {"oper=del"})
+	public String delete(ID id, RedirectAttributes redirectAttributes,
+						 HttpServletRequest request) {
+		checkAuth(OperationType.DELETE.getValue());
+		StringBuffer msgkey = new StringBuffer();
+		String message = null;
+		if (allowDeleteData(id, msgkey)) {
+			getBaseService().delete(id);
+			message = messageSource.getMessage(FerriswheelConstant.FW_MSG_SUCC, null,
+					RequestContextUtils.getLocale(request));
+		} else {
+			message = messageSource.getMessage(msgkey.toString(), null,
+					RequestContextUtils.getLocale(request));
+		}
+		redirectAttributes.addFlashAttribute("message", message);
+		return this.getRedirectView();
 	}
 
 	@Override
