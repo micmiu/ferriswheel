10a27402a6ef2fe864f26d175f6b144f4c9275bb
micmiu <sjsky007@gmail.com>
2016-01-15 13:42:38 +0800

    feat:add mybatis system moudle

---
+++ ferriswheel-demos/ferriswheel-demo-core-mybatis/src/main/java/com/micmiu/mvc/ferriswheel/examples/core/mapper/PermissionMapper.java
@@ -0,0 +1,11 @@
+package com.micmiu.mvc.ferriswheel.examples.core.mapper;
+
+
+import com.micmiu.mvc.ferriswheel.examples.core.entity.Permission;
+import com.micmiu.mvc.ferriswheel.orm.mybatis.mapper.GenerateMapper;
+
+/**
+ * User: <a href="http://micmiu.com">micmiu</a>
+ */
+public interface PermissionMapper extends GenerateMapper<Permission, Long> {
+}
---
+++ ferriswheel-demos/ferriswheel-demo-core-mybatis/src/main/java/com/micmiu/mvc/ferriswheel/examples/core/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.micmiu.mvc.ferriswheel.examples.core.mapper;
+
+
+import com.micmiu.mvc.ferriswheel.examples.core.entity.User;
+import com.micmiu.mvc.ferriswheel.orm.mybatis.mapper.GenerateMapper;
+
+/**
+ * User: <a href="http://micmiu.com">micmiu</a>
+ */
+public interface UserMapper extends GenerateMapper<User, Long> {
+}
